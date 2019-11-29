f1749a5e10f461c64e0a3a6e28b89f6aa3a608a6
Wojciech Pęciak <35200310+wagon15@users.noreply.github.com>
2019-11-29 13:00:49 +0100

    Fetching data by browser location coords

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
-import {weatherAPI} from './Weather.js';
-import {weatherHtml} from './UI.js';
+import {weatherAPI} from './WeatherAPI.js';
+import {weatherHtml} from './WeatherHTML.js';
 
 const   form = document.getElementById('weatherForm'),
         inputField = document.getElementById('city'),
@@ -11,6 +11,34 @@ const   form = document.getElementById('weatherForm'),
         btnForcast = document.getElementById('btnForcast'),
         btnAbout = document.getElementById('aboutBtn'),
         closeAboutBtn = document.getElementById('closeAboutBtn');
+let coords = null;
+
+// Check user position if allows
+document.addEventListener( 'DOMContentLoaded', () => {
+    navigator.geolocation.getCurrentPosition( localizationSucess );
+});
+
+// If user allows for localiztion display weather info
+function localizationSucess(position) {
+    const latitude  = position.coords.latitude;
+    const longitude = position.coords.longitude;
+
+    // Show loading spinner
+    weatherHtml.toggleSpinner();
+    weatherAPI.getCurrentByCoords(latitude, longitude)
+        .then( () => {
+            weatherHtml.toggleSpinner();
+            weatherHtml.showCurrentWeather(weatherAPI.currentData);
+
+            weatherAPI.getForcastByCoords(latitude, longitude)
+                .catch( () => {
+                    weatherHtml.errorNotify('The city data based on location was not found. Please try to find the city by entering name.');
+                });
+        })
+        .catch( () => {
+            weatherHtml.errorNotify('The city data based on location was not found. Please try to find the city by entering name.');
+        });
+}
 
 // Case when target submitted by the input field
 form.onsubmit = (event) => {
@@ -20,20 +48,20 @@ form.onsubmit = (event) => {
     if (inputField.value) {
         // Loading spinner
         weatherHtml.toggleSpinner();
-            weatherAPI.getCurrentByName(inputField.value)
-                .then( () => {
-                    weatherHtml.toggleSpinner();
-                    weatherHtml.showCurrentWeather(weatherAPI.currentData)
+        weatherAPI.getCurrentByName(inputField.value)
+            .then( () => {
+                weatherHtml.toggleSpinner();
+                weatherHtml.showCurrentWeather(weatherAPI.currentData);
 
-                    weatherAPI.getForcastByName(inputField.value)
-                        .catch( error => {
-                            weatherHtml.errorNotify('The city data was not found. Please try again or correct the city name.');
-                        });
-                        
-                })
-                .catch( error => {
-                    weatherHtml.errorNotify('The city data was not found. Please try again or correct the city name.');
-                });
+                weatherAPI.getForcastByName(inputField.value)
+                    .catch( error => {
+                        weatherHtml.errorNotify('The city data was not found. Please try again or correct the city name.');
+                    });
+                    
+            })
+            .catch( error => {
+                weatherHtml.errorNotify('The city data was not found. Please try again or correct the city name.');
+            });
 
     } else {
         weatherHtml.errorNotify('Check input field and name of desired city.')
